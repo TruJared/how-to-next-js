5ac9b1094303ef2ffe0a2576f3d54ffd6d1e9fef
Jared Truscott <jared@jaredmakes.com>
2023-07-16 12:58:27 -0400

    basic routing

---
+++ app/components/Navigation.tsx
@@ -0,0 +1,31 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export interface NavigationProps {
+  navigation: {
+    name: string;
+    url: string;
+  }[];
+}
+
+export default function Navigation({ ...props }: NavigationProps) {
+  const { navigation } = props;
+  const pathname = usePathname();
+
+  return (
+    <div className="w-full px-4 py-6 bg-red-400">
+      <ul className="max-w-2xl mx-auto flex flex-row justify-end items-center ">
+        {navigation.map((item, index) => {
+          const isActive = pathname === item.url;
+          const linkColor = isActive ? "text-white" : "b;lack";
+          return (
+            <li key={index} className={`px-5 hover:text-white ${linkColor}`}>
+              <Link href={item.url}>{item.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
