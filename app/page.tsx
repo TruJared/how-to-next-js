9ffc8ec735483629b319f299da5cc61d3ce98c5b
Jared Truscott <jared@jaredmakes.com>
2023-07-16 10:03:53 -0400

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-12">
+      <h1 className="text-5xl font-medium text-center capitalize">This is the home page</h1>
+    </main>
+  );
+}
---
+++ app/stuff/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-12">
+      <h1 className="text-5xl font-medium text-center capitalize">
+        Stuff
+      </h1>
+    </main>
+  );
+}
